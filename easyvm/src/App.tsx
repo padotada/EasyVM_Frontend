7f4902f86bec8357770412e334708824fd2242c9
Sophie Choi <103692454+sophieelissachoi@users.noreply.github.com>
2026-01-13 20:51:22 -0800

    created routes to separate pages

--- easyvm/src/App.tsx
+++ easyvm/src/App.tsx
@@ -1,9 +1,40 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Dashboard from "./pages/dashboard";
+import ExistingAllocation from "./pages/existing_allocation";
+import NewAllocation from "./pages/new_allocation";
+import Settings from "./pages/settings";
+import Login from "./pages/login";
 import "./App.css";
 
 function App() {
-	return <h1 className="text-blue-500"> hi </h1>;
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={<Login />}
+				/>
+				<Route
+					path="/dashboard"
+					element={<Dashboard />}
+				/>
+				<Route
+					path="/existing-allocation"
+					element={<ExistingAllocation />}
+				/>
+				<Route
+					path="/new-allocation"
+					element={<NewAllocation />}
+				/>
+				<Route
+					path="/settings"
+					element={<Settings />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
