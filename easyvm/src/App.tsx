1fa4f948617469d8c82435ff12fd981d73615774
Sophie Choi <103692454+sophieelissachoi@users.noreply.github.com>
2026-01-13 20:37:15 -0800

    tailwindcss works now

--- easyvm/src/App.tsx
+++ easyvm/src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-
-  return (
-    <Routes>
-      <Route></Route>
-      <Route></Route>
-      <Route></Route>
-      <Route></Route>
-      <Route></Route>
-    </Routes>
-  )
+	return <h1 className="text-blue-500"> hi </h1>;
 }
 
-export default App
+export default App;
